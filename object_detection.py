2f6d067d0593ca57d85837dc42023cfee35f7b9e
George Savill <gpsavill@gmail.com>
2019-04-23 18:46:34 +0100

    Add functionality to output multiple videos
    This commit allows multiple video files to be output, timestamped with
    the date of the object detection. Previously, all video frames with
    detected objects were stitched together and output as a single file.

--- object_detection.py
+++ object_detection.py
@@ -1,10 +1,25 @@
-import os
 import cv2
 import numpy as np
-import tensorflow as tf
+import os
 import sys
+import tensorflow as tf
+
+from datetime import datetime
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+
+
+def getcurrenttime():
+    now = datetime.now()
+    year = str(now.year)
+    month = str(now.month).zfill(2)
+    day = str(now.day).zfill(2)
+    hour = str(now.hour).zfill(2)
+    minute = str(now.minute).zfill(2)
+    second = str(now.second).zfill(2)
+    
+    return(year + "-" + month + "-" + day + "-" + hour + "-" + minute + "-" + second)
+
 
 VIDEO_STREAM_URL = "http://username:password@192.168.1.93:8080/video"
 PATH_TO_GRAPH = "./model/frozen_inference_graph.pb"
@@ -34,15 +49,18 @@ num_detections = object_detection_graph.get_tensor_by_name('num_detections:0')
 
 video_stream = cv2.VideoCapture(VIDEO_STREAM_URL)
 
+
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-output_video = cv2.VideoWriter('output.avi', fourcc, 20.0, (720,720))
+# output_video = cv2.VideoWriter(getcurrenttime() + '.avi', fourcc, 20.0, (720,720))
+
+empty_frame_count = 10
 
 while(video_stream.isOpened()):
     frame_recieved, frame = video_stream.read()
     # frame = frame[350:900, 538:1088]
 
     if frame_recieved:
-        
+
         frame_expanded = np.expand_dims(frame, axis=0)
 
         (boxes, scores, classes, num) = sess.run(
@@ -67,9 +85,21 @@ while(video_stream.isOpened()):
                 if squeezed_classes[i] in category_index.keys():
                     class_name = category_index[np.squeeze(classes).astype(np.int32)[i]]['name']
                     if (class_name == "person"):
-                        output_video.write(frame)    
+                        if (empty_frame_count > 5):
+                            output_video = cv2.VideoWriter(getcurrenttime() + '.avi', fourcc, 20.0, (720,720))
+                            output_video.write(frame)
+                        else:
+                            output_video.write(frame)
+
+                        empty_frame_count = 0 
+
+        empty_frame_count += 1
+
 
         cv2.imshow('Object detector', frame)
+        print(empty_frame_count)
+    else:
+        print("frame skipped")
 
     if cv2.waitKey(1) == ord('q'):
         break
