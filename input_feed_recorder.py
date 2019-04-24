1c741e67d32064cc3a5d6df4cb7af614222e64bb
George Savill <gpsavill@gmail.com>
2019-04-24 21:09:47 +0100

    Add basic image recording functionality
    This was completed with the goal of making image_input_feed_processor.py
    a general image recording script. Used, for example, for generating
    training data/demo videos.

--- input_feed_recorder.py
+++ input_feed_recorder.py
@@ -4,12 +4,16 @@ import cv2
 
 video_feed = cv2.VideoCapture("http://username:password@192.168.1.93:8080/video")
 
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+output_video = cv2.VideoWriter( 'output.avi', fourcc, 20.0, (720,720))
+
 while (video_feed.isOpened()):
     feed_opened, frame = video_feed.read()
 
     if not feed_opened:
         break
 
+    output_video.write(frame)
     # Crop image to region of interest
     # frame = frame[350:900, 538:1088]
 
@@ -18,5 +22,6 @@ while (video_feed.isOpened()):
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+output_video.release()
 video_feed.release()
 cv2.destroyAllWindows()
